700adc65a243d768399c4d8e25af4779687e0e09
yanatsb <cybina96@mail.ru>
2022-04-26 21:42:14 +0300

    -

---
+++ css/js/main.js
@@ -0,0 +1,7 @@
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.header__nav');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
